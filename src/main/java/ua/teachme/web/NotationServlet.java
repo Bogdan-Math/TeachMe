509ee2f7464b60c0b92c46a24abaa2b3bbc6c0bc
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-04-17 20:48:58 +0300

    add logging by logback with slf4j api

--- src/main/java/ua/teachme/web/NotationServlet.java
+++ src/main/java/ua/teachme/web/NotationServlet.java
@@ -1,16 +1,24 @@
 package ua.teachme.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class NotationServlet extends HttpServlet {
+
+    private static final Logger LOG = getLogger(NotationServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/notationList.jsp").forward(request, response);
+        LOG.debug("redirect to notations");
+        //request.getRequestDispatcher("/notationList.jsp").forward(request, response);
+        response.sendRedirect("notationList.jsp");
     }
 
 }
