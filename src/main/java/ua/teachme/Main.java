b18f9782aa3bee1272e680e0e4fdbfd2f7fb7310
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-04-20 16:05:49 +0300

    add spring context

--- src/main/java/ua/teachme/Main.java
+++ src/main/java/ua/teachme/Main.java
@@ -2,6 +2,7 @@ package ua.teachme;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.teachme.repository.NotationRepository;
 
 import java.util.Arrays;
 
@@ -12,6 +13,15 @@ public class Main {
 
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-app.xml");
         System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+
+        //get bean by name
+        NotationRepository notationRepository1 = (NotationRepository) context.getBean("notationRepository");
+        System.out.println(notationRepository1);
+
+        //get bean by class
+        NotationRepository notationRepository2 = context.getBean(NotationRepository.class);
+        System.out.println(notationRepository2);
+
         context.close();
     }
 
