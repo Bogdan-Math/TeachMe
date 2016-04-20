e9790911fe9ef91322b9c263907273e0d4c7f893
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-04-20 16:29:21 +0300

    add spring context

--- src/main/java/ua/teachme/Main.java
+++ src/main/java/ua/teachme/Main.java
@@ -2,8 +2,10 @@ package ua.teachme;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.teachme.model.Notation;
 import ua.teachme.repository.NotationRepository;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class Main {
@@ -16,13 +18,15 @@ public class Main {
 
         //get bean by name
         NotationRepository notationRepository1 = (NotationRepository) context.getBean("notationRepository");
-        System.out.println(notationRepository1);
+        System.out.println(notationRepository1.save(new Notation("name1", "url1", "desc1", 0, LocalDateTime.now())));
 
         //get bean by class
         NotationRepository notationRepository2 = context.getBean(NotationRepository.class);
-        System.out.println(notationRepository2);
+        System.out.println(notationRepository2.save(new Notation("name2", "url2", "desc2", 0, LocalDateTime.now())));
 
         context.close();
+
+
     }
 
 
