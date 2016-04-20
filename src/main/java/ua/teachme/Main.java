3978f59463159a18dc754ff93821ced1e49cb322
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-04-20 15:53:37 +0300

    add spring context

--- src/main/java/ua/teachme/Main.java
+++ src/main/java/ua/teachme/Main.java
@@ -1,8 +1,19 @@
 package ua.teachme;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello TeachMe Enterprise!");
+
+        System.out.println("Hello TeachMe Enterprise!");
+
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+        context.close();
     }
+
 
 }
