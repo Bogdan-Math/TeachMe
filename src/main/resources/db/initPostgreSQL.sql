b8d6615e3afaca8bc7f7a532ac3e965f46373cc3
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-04-29 18:52:16 +0300

    link notations and users in db scripts

--- src/main/resources/db/initPostgreSQL.sql
+++ src/main/resources/db/initPostgreSQL.sql
@@ -3,24 +3,30 @@ DROP SEQUENCE IF EXISTS global_sequence CASCADE;
 CREATE SEQUENCE global_sequence START 1000000;
 
 /*init users table*/
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users
 (
-  id                         INTEGER PRIMARY KEY DEFAULT nextval('global_sequence'),
+  id                         INTEGER DEFAULT nextval('global_sequence'),
   name                       VARCHAR NOT NULL,
   password                   VARCHAR NOT NULL,
   email                      VARCHAR NOT NULL,
   max_hours_per_day          INTEGER DEFAULT 8 NOT NULL,
-  registered_date_and_time   TIMESTAMP DEFAULT now()
+  registered_date_and_time   TIMESTAMP DEFAULT now(),
+
+  PRIMARY KEY (id)
 );
 
 /*init notations table*/
 DROP TABLE if EXISTS notations;
 CREATE TABLE notations(
-  id                      INTEGER PRIMARY KEY DEFAULT nextval('global_sequence'),
+  id                      INTEGER DEFAULT nextval('global_sequence'),
   name                    VARCHAR NOT NULL,
   url                     VARCHAR NOT NULL,
   description             TEXT,
   hours                   INTEGER DEFAULT 2,
-  created_date_and_time   TIMESTAMP DEFAULT now()
+  created_date_and_time   TIMESTAMP DEFAULT now(),
+  user_id                 INTEGER NOT NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id)   REFERENCES users(id) ON DELETE CASCADE
 );
--- src/main/resources/db/populatePostgreSQL.sql
+++ src/main/resources/db/populatePostgreSQL.sql
@@ -1,5 +1,5 @@
 /*restart global_sequence*/
-ALTER SEQUENCE global_sequence RESTART WITH 1000000;
+ALTER SEQUENCE global_sequence RESTART WITH 1000001;
 
 /*populate users table*/
 DELETE FROM users;
@@ -16,11 +16,14 @@ VALUES ('Admin', 'password', 'admin@gmail.com');
 /*populate notations table*/
 DELETE FROM notations;
 
-INSERT INTO notations (name, url, description, hours)
-VALUES ('notations1', 'url1', 'desc1', 1);
+INSERT INTO notations (user_id, name, url, description, hours)
+VALUES (1000001, 'notations1', 'url1', 'desc1', 1),
+       (1000001, 'notations2', 'url2', 'desc2', 2);
 
-INSERT INTO notations (name, url, description, hours)
-VALUES ('notations2', 'url2', 'desc2', 2);
+INSERT INTO notations (user_id, name, url, description, hours)
+VALUES (1000002, 'notations3', 'url3', 'desc3', 3),
+       (1000002, 'notations4', 'url4', 'desc4', 4);
 
-INSERT INTO notations (name, url, description, hours)
-VALUES ('notations3', 'url3', 'desc3', 3);
+INSERT INTO notations (user_id, name, url, description, hours)
+VALUES (1000003, 'notations5', 'url5', 'desc5', 5),
+       (1000003, 'notations6', 'url6', 'desc6', 6);
