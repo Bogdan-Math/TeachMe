ca4e1f0ef23af96ce36c013376d09d52cbf31e98
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-05-12 20:50:06 +0300

    clear controller layer tests

--- src/test/java/ua/teachme/web/notation/NotationControllerTest.java
+++ src/test/java/ua/teachme/web/notation/NotationControllerTest.java
@@ -18,20 +18,11 @@ import ua.teachme.util.notation.NotationUtil;
 
 import static org.junit.Assert.*;
 
-//tests with Spring
-@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/db-connect.xml", "classpath:spring/db-behaviour.xml"})
-@RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles({ConnectTo.POSTGRESQL, WorkBy.JPA})
-@Sql(scripts = Populate.POSTGRESQL) //execute before every test in this class
-//todo: add more requirements and checks to tests
 public class NotationControllerTest {
-
-    @Autowired
-    private NotationController notationController;
 
     @Before
     public void setUp(){
-        notationController.evictCache();
+
     }
 
     @After
@@ -41,44 +32,32 @@ public class NotationControllerTest {
 
     @Test
     public void testNotationController(){
-        assertNotNull(notationController);
+
     }
 
     @Test
     public void testGetAll() throws Exception {
-        assertEquals(6, notationController.getAll().size());
+
     }
 
     @Test
     public void testSave() throws Exception {
-        assertEquals(6, notationController.getAll().size());
-        assertEquals(NotationUtil.newNotation, notationController.save(NotationUtil.newNotation));
-        assertEquals(7, notationController.getAll().size());
+
     }
 
-    @Test//(expected = EntityNotFoundException.class)
+    @Test
     public void testGet() throws Exception {
-        notationController.get(1000007);
-        //notationController.get(-10);
+
     }
 
     @Test
     public void testDelete() throws Exception {
-        assertEquals(6, notationController.getAll().size());
-        notationController.delete(1000007);
-        assertEquals(5, notationController.getAll().size());
+
     }
 
     @Test
     @Transactional
     public void testGetBetween() throws Exception {
-        Notation notation = notationController.get(1000007);
-        assertEquals(
-                2,
-                notationController.getBetween(
-                        notation.getDate(), notation.getTime(),
-                        notation.getDate(), notation.getTime()
-                ).size()
-        );
+
     }
 }
