4e60a86968490a8a11050e5f117ab25f921b0d48
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-05-12 19:33:40 +0300

    fix input test data

--- src/main/java/ua/teachme/util/user/UserUtil.java
+++ src/main/java/ua/teachme/util/user/UserUtil.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 public class UserUtil {
 
+    public static User newUser = new User("NewUser", "password", "new-user@gmail.com", NotationUtil.hours);
+    public static User equalUser = new User("NewUser", "password", "new-user@gmail.com", NotationUtil.hours);
+
     public static User admin = new User("Admin", "admin-password", "admin@gmail.com", NotationUtil.hours);
 
     public static User user = new User("User", "user-password", "user@gmail.com", NotationUtil.hours);
 
     public static User anonymous = new User("Anonymous", "anonymous-password", "anonymous@gmail.com", NotationUtil.hours);
-
-    public static User newUser = new User("NewUser", "user-password", "new-user@gmail.com", NotationUtil.hours);
-    public static User equalUser = new User("NewUser", "user-password", "new-user@gmail.com", NotationUtil.hours);
 
     public static List<User> users = Arrays.asList(
             admin,
