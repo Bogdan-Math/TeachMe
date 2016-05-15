71fdb81bba2f225aa9216fb125e1e58bdc2d3c62
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-05-15 03:06:45 +0300

    add spring mvc

--- src/main/java/ua/teachme/web/RootController.java
+++ src/main/java/ua/teachme/web/RootController.java
@@ -5,13 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.teachme.service.NotationService;
 import ua.teachme.service.UserService;
+import ua.teachme.util.notation.NotationUtil;
 
 @Controller
 public class RootController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private NotationService notationService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root(){
@@ -27,5 +32,11 @@ public class RootController {
     public String users(Model model){
         model.addAttribute("users", userService.getAll());
         return "users";
+    }
+
+    @RequestMapping(value = "notations", method = RequestMethod.GET)
+    public String notations(Model model){
+        model.addAttribute("notations", NotationUtil.getFilteredWithExceed(notationService.getAll(), NotationUtil.hours));
+        return "notations";
     }
 }
