f596ac2730ebfa7f6827f67b107e3f6f50899106
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-05-19 16:25:47 +0300

    add empty value

--- src/main/java/ua/teachme/web/user/UserController.java
+++ src/main/java/ua/teachme/web/user/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserController extends AbstractUserController{
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String getAll(Model model) {
         model.addAttribute("users", super.getAll());
         return "users";
