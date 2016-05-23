12679cbd5fae211bb3f5c30dfde5c08c8c6a22eb
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-05-23 13:12:00 +0300

    add method to rest notation controller to find notations between 2 dates

--- src/main/java/ua/teachme/web/notation/NotationRestController.java
+++ src/main/java/ua/teachme/web/notation/NotationRestController.java
@@ -7,6 +7,7 @@ import ua.teachme.dto.NotationExceed;
 import ua.teachme.model.Notation;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
@@ -37,8 +38,13 @@ public class NotationRestController extends AbstractNotationController{
     }
 
     @Override
-//    @RequestMapping(value = "/between", method = RequestMethod.GET)
-    public List<NotationExceed> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+    @RequestMapping(value = "/between", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<NotationExceed> getBetween(
+            @RequestParam(value = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(value = "startTime") @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTime,
+            @RequestParam(value = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            @RequestParam(value = "endTime") @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime endTime
+    ) {
         //todo: fix error !!!
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
