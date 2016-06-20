4bcb82616f7a0055e8e655f62d8fe0297109f9f2
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-06-20 19:50:48 +0300

    add mapper to correct converting date and time to json

---
+++ src/main/java/ua/teachme/util/json/JsonDateTimeMapper.java
@@ -0,0 +1,12 @@
+package ua.teachme.util.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonDateTimeMapper extends ObjectMapper{
+    public JsonDateTimeMapper() {
+        registerModule(new JavaTimeModule());
+        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+}
