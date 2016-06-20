28495164caa122fc39ee2868fab3f86ea98de225
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-06-20 15:31:04 +0300

    add basic registration rest controller

---
+++ src/main/java/ua/teachme/web/controllers/rest/RegistrationRestController.java
@@ -0,0 +1,10 @@
+package ua.teachme.web.controllers.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/rest/registration")
+public class RegistrationRestController {
+    //todo: add registration by rest
+}
