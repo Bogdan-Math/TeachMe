d07d9631e1440f65c72c4eada77f2082b6450fe0
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-06-21 16:43:32 +0300

    fix abstract registration controller

--- src/main/java/ua/teachme/web/controllers/AbstractRegistrationController.java
+++ src/main/java/ua/teachme/web/controllers/AbstractRegistrationController.java
@@ -1,15 +1,17 @@
 package ua.teachme.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import ua.teachme.dto.UserTO;
 import ua.teachme.model.User;
 import ua.teachme.service.UserService;
+import ua.teachme.util.user.UserUtil;
 
 public abstract class AbstractRegistrationController {
 
     @Autowired
     private UserService userService;
 
-    public User save(User user){
-        return userService.save(user);
+    public User save(UserTO userTO){
+        return userService.save((UserUtil.saveUser(userTO)));
     }
 }
