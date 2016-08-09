552ed066c20a1a0484f75922a70695a7dd70c8a8
Bogdan-Math <bogdan.math.stepanov@gmail.com>
2016-08-09 09:57:06 +0300

    add simple tests to jpa repository

--- src/test/java/ua/teachme/repository/jpa/JpaNotationRepositoryImplTest.java
+++ src/test/java/ua/teachme/repository/jpa/JpaNotationRepositoryImplTest.java
@@ -4,11 +4,27 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.teachme.profiles.ConnectTo;
+import ua.teachme.profiles.PopulateDB;
+import ua.teachme.profiles.WorkBy;
+import ua.teachme.repository.NotationRepository;
 
 import static org.junit.Assert.*;
 
-@Ignore //todo: add tests
+@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/db-connect.xml", "classpath:spring/db-behaviour.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles({ConnectTo.POSTGRESQL, WorkBy.JPA})
+@Sql(scripts = PopulateDB.POSTGRESQL) //execute before every test in this class
 public class JpaNotationRepositoryImplTest {
+
+    @Autowired
+    private NotationRepository notationRepository;
 
     @Before
     public void setUp() throws Exception {
@@ -21,13 +37,13 @@ public class JpaNotationRepositoryImplTest {
     }
 
     @Test
-    public void testGetBetween() throws Exception {
-
+    public void testNotationRepository(){
+        assertNotNull(notationRepository);
     }
 
     @Test
     public void testGetAll() throws Exception {
-
+        assertEquals(6, notationRepository.getAll().size());
     }
 
     @Test
@@ -42,6 +58,11 @@ public class JpaNotationRepositoryImplTest {
 
     @Test
     public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetween() throws Exception {
 
     }
 }
